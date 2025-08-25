dc88090f34bdbad141a0aef6830536f945d30f5b
ericaokeh <157079075+ericaokeh@users.noreply.github.com>
2025-08-24 20:28:53 -0400

    add java source files

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+Erica Okeh
+*/
+
+public class HelloWor{
+    public static void main (String[] args){
+        System.out.println("hello, world!");
+    }
+}
